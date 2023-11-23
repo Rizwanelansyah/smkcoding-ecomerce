6ab5311d73e70adc56e35ddb7d1983c04109678a
Rizwanelansyah <111105347+Rizwanelansyah@users.noreply.github.com>
2023-11-23 17:35:27 +0700

    update web

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,5 +1,10 @@
 <script lang="ts" setup>
 import type { Products } from "~/types/products";
+
+definePageMeta({
+  middleware: ["user-access"],
+});
+
 const products = ref<Products[]>([]);
 const totalPrice = computed(() => {
   return products.value
@@ -25,13 +30,15 @@ const removeCart = (id: number) => {
     <div class="container">
       <div class="py-10 flex gap-6">
         <div class="w-[70%]">
-          <div class="flex justify-between items-center pb-7 border-b border-gray- 300 mb-6">
+          <div
+            class="flex justify-between items-center pb-7 border-b border-gray- 300 mb-6"
+          >
             <h1 class="text-3xl font-medium">Shopping Cart</h1>
             <p class="text-3xl font-medium">{{ products.length }} Items</p>
           </div>
           <div v-if="products.length > 0" class="flex flex-col gap-6">
             <template v-for="(item, index) in products" :key="index">
-              <CardsCardCart :product="item" @removeCart="removeCart"/>
+              <CardsCardCart :product="item" @removeCart="removeCart" />
             </template>
           </div>
           <div v-else>
@@ -42,21 +49,30 @@ const removeCart = (id: number) => {
           <h3 class="text-xl font-medium mb-6">Order Summary</h3>
           <div class="flex flex-col gap-3 border-b border-gray-300 pb-4">
             <div v-if="products.length > 0">
-              <div v-for="(item, index) in products" :key="index" class="flex gap-4 items-center">
+              <div
+                v-for="(item, index) in products"
+                :key="index"
+                class="flex gap-4 items-center"
+              >
                 <span class="text-limit limit-1 text-sm">{{ item.name }}</span>
                 <span class="text-sm font-semibold">${{ item.price }}</span>
               </div>
             </div>
             <div v-else>
-              <p class="text-sm text-center font-light">There are no to orders yet</p>
+              <p class="text-sm text-center font-light">
+                There are no to orders yet
+              </p>
             </div>
           </div>
           <div class="pt-4 flex items-center justify-between mb-6">
             <span class="text-base">Total</span>
             <span class="text-base font-bold">${{ totalPrice }}</span>
           </div>
-          <button class="bg-blue-600 text-white text-base font-bold w-full py-2
-          rounded-lg">Checkout</button>
+          <button
+            class="bg-blue-600 text-white text-base font-bold w-full py-2 rounded-lg"
+          >
+            Checkout
+          </button>
         </div>
       </div>
     </div>
